0104286f85019842b64fa9b84f67894b1a3becb5
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-06-14 09:20:50 -0500

    About section added to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,31 +5,69 @@ import Link from "next/link";
 import signature from "../../public/assets/images/signature.png";
 import BtnPrimaryXl from "@/components/Buttons/BtnPrimaryXl";
 import heroImage from "../../public/assets/images/hero-image.png";
+import aboutImage from "../../public/assets/images/about.png";
+import BtnPrimary from "@/components/Buttons/BtnPrimary";
 export default function Home() {
   return (
     <main className={`${styles.main} `}>
-      <Image
-        className={`${styles.hero__image} section__bg-img`}
-        src={heroImage}
-      />
-      <div className={styles.section__wrapper}>
-        <div className="section-card section-card--glass rounded">
-          <Image
-            className={styles.signature}
-            src={signature}
-            alt="Nancy Hodnefield's signature"
-          />
-          <h1 className="title title--lg">
-            Elevate Your Career <br /> with Confidence
-          </h1>
-          <h2 className="title--sm">
-            Guiding Ambitious Young Women to Success
-          </h2>
-          <Link href={"/"}>
-            <BtnPrimaryXl btnText={"Ignite Your Potential Now"}></BtnPrimaryXl>
-          </Link>{" "}
+      <section className={`${styles.hero__wrapper} section__container`}>
+        <Image
+          className={`${styles.hero__image} section__bg-img`}
+          src={heroImage}
+        />
+        <div className={`section-card__wrapper ${styles.section__wrapper}`}>
+          <div className="section-card section-card--glass rounded">
+            <Image
+              className={styles.signature}
+              src={signature}
+              alt="Nancy Hodnefield's signature"
+            />
+            <h1 className="title title--lg">
+              Elevate Your Career <br /> with Confidence
+            </h1>
+            <h2 className="title--sm">
+              Guiding Ambitious Young Women to Success
+            </h2>
+            <Link href={"/"}>
+              <BtnPrimaryXl
+                btnText={"Ignite Your Potential Now"}
+              ></BtnPrimaryXl>
+            </Link>{" "}
+          </div>
         </div>
-      </div>
+      </section>
+
+      {/* About section */}
+      <section className="section__container">
+        <div className={`section-card__wrapper ${styles.aboutcard__wrapper}`}>
+          {" "}
+          <Image
+            src={aboutImage}
+            alt="Nancy Hodnefield wearing a sweater smiling."
+            className={`cover rounded ${styles.about__img}`}
+          />
+          <div
+            className={`section-card section-card--white rounded ${styles.about__card}`}
+          >
+            <h2 className=" title title--md">Hi, I'm Nancy</h2>
+            <p className="section-card__text section-card__text-mgsm">
+              Welcome! I'm, a professional coach based in the beautiful state of
+              Minnesota. With a background in human resources and a passion for
+              empowering young female professionals, I am dedicated to helping
+              you unlock your full potential and achieve your career goals.
+            </p>
+            <p className="section-card__text section-card__text-mglg">
+              I bring a deep understanding of organizational dynamics, workplace
+              challenges, and effective strategies for career advancement. I'll
+              share valuable insights and practical tools to navigate the
+              corporate landscape with confidence and authenticity.
+            </p>
+            <Link href={""}>
+              <BtnPrimary btnText={"My Story"} />
+            </Link>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
